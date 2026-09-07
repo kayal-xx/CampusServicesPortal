b39ef9ba61be76430f5f8afafacad31a0da21701
kayal-xx <kayaliniyansilvester@gmail.com>
2026-09-07 10:41:06 +0530

    Implement Member 3 notifications frontend

--- frontend/campus-services-portal/src/app/app.routes.ts
+++ frontend/campus-services-portal/src/app/app.routes.ts
@@ -1,20 +1,26 @@
 import { Routes } from '@angular/router';
 import { FeeList } from './features/fees/fee-list/fee-list';
 import { FeeManagement } from './features/fees/fee-management/fee-management';
+import { NotificationList } from './features/notifications/notification-list/notification-list';
 
 
 export const routes: Routes = [
-  {
-    path: 'fees',
-    component: FeeList
-  },
-  {
-  path: 'admin/fees',
-  component: FeeManagement
-},
-  {
-    path: '',
-    redirectTo: 'fees',
-    pathMatch: 'full'
-  }
+    {
+        path: 'fees',
+        component: FeeList
+    },
+    {
+        path: 'admin/fees',
+        component: FeeManagement
+    },
+    {
+        path: '',
+        redirectTo: 'fees',
+        pathMatch: 'full'
+    },
+    {
+        path: 'notifications',
+        component: NotificationList
+    }
+
 ];
