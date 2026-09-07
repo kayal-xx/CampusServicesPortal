164bdcb081b24c05d2434d1d5ae570d4fe17806d
sethasiva <sethasiva02@gmail.com>
2026-09-07 11:57:51 +0530

    Implement authentication frontend and login UI

--- frontend/campus-services-portal/src/app/app.routes.ts
+++ frontend/campus-services-portal/src/app/app.routes.ts
@@ -1,18 +1,34 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
 import { EventList } from './features/events/event-list/event-list';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    component: Login,
+    title: 'Login | Campus Services Portal'
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard],
+    title: 'Dashboard | Campus Services Portal'
+  },
+  {
     path: 'events',
-    component: EventList
+    component: EventList,
+    canActivate: [authGuard],
+    title: 'Events | Campus Services Portal'
   },
   {
     path: '',
-    redirectTo: 'events',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: '**',
-    redirectTo: 'events'
+    redirectTo: 'login'
   }
 ];
